f920044ba11dfed21305841659f6e15fc653d7c8
Pititorial <pititorial@gmail.com>
2025-04-11 16:21:55 +0700

    เพิ่มฟีเจอร์การสมัครสมาชิกผู้ใช้ โดยสร้าง UsersController, โมเดล User พร้อมการตรวจสอบความถูกต้อง และไฟล์มุมมองสำหรับฟอร์มสมัครสมาชิก

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+  
+  validates :email, presence: true, uniqueness: true, 
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
 end
